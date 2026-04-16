18c3457ea6e46216c9bea265e0502e92fe101513
mechael7 <mechaelmandausanduo7@gmail.com>
2026-04-17 03:27:01 +0700

    add style in header

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,16 +10,18 @@
 <body class="bg-[#f8fafc]">
     <header class="flex bg-white items-center">
         <h2 class="m-4 text-4xl text-[#4f46e5]  font-bold">Plaform</h2>
-        <div class="nav-bar m-auto" >
-        <a href="" class="text-[#e0e3e5] m-2 ">Home</a>
-        <a href="" class="text-[#e0e3e5] m-2">Categories</a>
-        <a href="" class="text-[#e0e3e5]">About</a>
+        <div class="nav-bar flex m-auto" >
+            <a href="" class="text-[#8E9193] m-2 ">Home</a>
+            <a href="" class="text-[#8E9193] m-2">Categories</a>
+            <a href="" class="text-[#8E9193] m-2">About</a>
         </div>
-        <a href="">Login</a>
-        <a href="">Dasboard</a>
+        <div class="nav-login-dash  p-4">
+            <a href="" class="text-[#3323CC] bg-[#A9ABAD] border-black pt-2 pb-2 pl-4 pr-4 mr-4 rounded-lg">Login</a>
+            <a href="" class="font-bold text-white bg-[#3323cc] pt-2 pb-2 pl-4 pr-5 rounded-lg">Dasboard</a>
+        </div>
     </header>
     <main>
-        <div class="card-logi">
+        <div class="card-login w-80 h-4/5 border-2 border-black flex flex-col justify-self-center">
             <img src="" alt="">
             <h3>Welcome Back</h3>
             <h5>Continue your ediorial journey</h5>
