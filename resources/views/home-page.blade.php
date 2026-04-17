b09d3d88d469168b79ec0484e085ccdb24428d0b
mechael7 <mechaelmandausanduo7@gmail.com>
2026-04-17 21:52:03 +0700

    add struktur home-page

---
+++ resources/views/home-page.blade.php
@@ -0,0 +1,17 @@
+<x-template>
+    <main>
+        <p>Current Issue</p>
+        <h2>Perspective in Modern Design.</h2>
+        <h4>Curated insights on architectural digital design, minimalist workflows, and the future of editorial experiences.</h4>
+        <section>
+            <img src="" alt="">
+            <p></p>
+            <h2></h2>
+            <h4></h4>
+            <img src="" alt="">
+            <h3></h3>
+            <p></p>
+            <p></p>
+        </section>
+    </main>
+</x-template>
